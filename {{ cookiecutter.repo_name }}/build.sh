ae9abd6ac3391d0aae7cdbee04dadb8e28f14ed7
GatlenCulp <GatlenCulp@gmail.com>
2025-02-06 21:57:49 -0500

    feat: enhance script robustness, rename logo, and add aux files

--- {{ cookiecutter.repo_name }}/build.sh
+++ {{ cookiecutter.repo_name }}/build.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+# Exit on any error
+set -e
+# Exit on any undefined variable
+set -u
+# Exit if any command in a pipe fails
+set -o pipefail
+
+COMPOSE=compose.gatpack.json
+
+COVER_LATEX_TEMPLATE=cover/cover.jinja.tex
+COVER_LATEX=cover/cover.tex
+COVER_PDF=cover/cover.pdf
+
+READINGS_PDFS=readings/*.pdf
+
+FURTHER_READINGS_LATEX_TEMPLATE=further_readings/further_readings.jinja.tex
+FURTHER_READINGS_LATEX=further_readings/further_readings.tex
+FURTHER_READINGS_PDF=further_readings/further_readings.pdf
+
+OUTPUT_PDF=packet.pdf
+
 # Build Cover Page
-gatpack render cover.jinja.tex cover.tex config.gatpack.yaml
-gatpack build cover.tex cover.pdf
+rm -f $COVER_LATEX
+rm -f $COVER_PDF
+gatpack render $COVER_LATEX_TEMPLATE $COVER_LATEX $COMPOSE
+gatpack build $COVER_LATEX $COVER_PDF
 
 # Build Further Readings Page
-gatpack render further_readings.jinja.tex further_readings.tex config.gatpack.yaml
-gatpack build further_readings.tex further_readings.pdf
+rm -f $FURTHER_READINGS_LATEX
+rm -f $FURTHER_READINGS_PDF
+gatpack render $FURTHER_READINGS_LATEX_TEMPLATE $FURTHER_READINGS_LATEX $COMPOSE
+gatpack build $FURTHER_READINGS_LATEX $FURTHER_READINGS_PDF
 
 # Combine all readings into "packet.pdf"
-gatpack combine cover.pdf further_readings.pdf packet.pdf
+rm -f $OUTPUT_PDF
+gatpack combine $COVER_PDF $READINGS_PDFS $FURTHER_READINGS_PDF $OUTPUT_PDF
 
 # Add footer to the readings
 # gatpack footer packet.pdf "Page {{ n }} of {{ N }}" packet-final.pdf
